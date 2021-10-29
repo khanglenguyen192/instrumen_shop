79b329933dcee5b01abb7c2276437b0ec5d3829b
Hien_Nguyen <73571967+ngochienhv@users.noreply.github.com>
2021-10-30 00:23:08 +0700

    Update product.js

--- productList/product.js
+++ productList/product.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import styles from "styled-components";
 import { Link } from 'react-router-dom';
 import { StarRating } from './StarRating';
 
-const ProductWrapper = styles.div`
 
-`
+
+
 export default class Product extends Component {
     render() {
         const { id, title, img, price } = this.props.product;
         return (
-            <ProductWrapper className="col-8 col-md-5 col-lg-3 my-3">
+            <div className="col-8 col-md-5 col-lg-3 my-3 col-grid">
                 <div className="card">
                     <div className="img-container p-5">
                         <Link to="/">
@@ -21,14 +20,14 @@ export default class Product extends Component {
                     <div className="card-footer d-flex justify-content-center">
                         <ul className="list-group list-group-flush text-center">
                             <StarRating value={3} />
-                            <Link to="/">
+                            <Link to="/" className="link">
                             <li className="list-group-item"><p className="align-self-center mb-0">{title}</p></li>
                             </Link>
                             <li className="list-group-item"><h5 className="font-italic mb-0">{price}</h5></li>
                         </ul>
                     </div>
                 </div>
-            </ProductWrapper>
+            </div>
         )
     }
 }
