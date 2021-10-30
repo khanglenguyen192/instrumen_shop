0b53de1346a524459fdfa90844aced086df02b0f
Le Nguyen Khang <khang.lenguyen192@gmail.com>
2021-10-30 16:12:52 +0700

    update footer, navbar, searchbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Introduction from "./pages/introduction";
+import Product from "./pages/product";
+import Services from "./pages/services";
+import Posts from "./pages/posts";
+import Promotion from "./pages/promotion";
+import Contact from "./pages/contact";
+import Installment from "./pages/installment";
+import { FooterContainer } from "./container/footer";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <SearchBar />
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/introduction" exact component={Introduction} />
+          <Route path="/product" exact component={Product} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/posts" exact component={Posts} />
+          <Route path="/promotion" exact component={Promotion} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/installment" exact component={Installment} />
+        </Switch>
+      </Router>
+
+      <FooterContainer />
     </div>
   );
 }
