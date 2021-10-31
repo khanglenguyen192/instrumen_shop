da6b39e3ad2b997b466ee7cf6e2b16cfd8f1781e
Le Nguyen Khang <khang.lenguyen192@gmail.com>
2021-11-01 02:12:28 +0700

    refactor code

--- src/components/products/ButtonHover.js
+++ src/components/products/ButtonHover.js
@@ -1,20 +1,27 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export default function ShowButtonHover() {
-    const [style, setStyle] = useState({display: 'none'});
+  const [style, setStyle] = useState({ display: "none" });
 
-    return (
-        <div className="App">
-            <div style={{border: '1px solid gray', width: 300, height: 300, padding: 10, margin: 100}}
-                 onMouseEnter={e => {
-                     setStyle({display: 'block'});
-                 }}
-                 onMouseLeave={e => {
-                     setStyle({display: 'none'})
-                 }}
-            >
-                <button style={style}>Click</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="App">
+      <div
+        style={{
+          border: "1px solid gray",
+          width: 300,
+          height: 300,
+          padding: 10,
+          margin: 100,
+        }}
+        onMouseEnter={(e) => {
+          setStyle({ display: "block" });
+        }}
+        onMouseLeave={(e) => {
+          setStyle({ display: "none" });
+        }}
+      >
+        <button style={style}>Click</button>
+      </div>
+    </div>
+  );
 }
--- src/components/products/product.js
+++ src/components/products/product.js
@@ -1,31 +1,35 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { ProductWrapper } from "../../container/ProductWrapper";
-import { StarRating } from '../starRating/StarRating';
+import { StarRating } from "../starRating/StarRating";
 
 export default class Product extends Component {
-    render() {
-        const { id, title, img, price } = this.props.product;
-        return (
-            <ProductWrapper className="col-8 col-md-5 col-lg-3 my-3 col-grid">
-                <div className="card prodCard">
-                    <div className="img-container p-5">
-                        <Link to="/">
-                            <img src={img} alt="products" className="card-img-top" id={id}/>
-                        </Link>
-                        <button className="cart-btn btn btn-sm">Thêm vào giỏ hàng</button>
-                    </div>
-                    <div className="card-footer d-flex justify-content-center">
-                        <ul className="list-group list-group-flush text-center">
-                            <StarRating value={3} />
-                            <Link to="/" className="link">
-                            <li className="list-group-item"><p className="align-self-center mb-0">{title}</p></li>
-                            </Link>
-                            <li className="list-group-item"><h5 className="font-italic mb-0">{price}</h5></li>
-                        </ul>
-                    </div>
-                </div>
-            </ProductWrapper>
-        )
-    }
+  render() {
+    const { id, title, img, price } = this.props.product;
+    return (
+      <ProductWrapper className="col-8 col-md-5 col-lg-3 my-3 col-grid">
+        <div className="card prodCard">
+          <div className="img-container p-5">
+            <Link to="/">
+              <img src={img} alt="products" className="card-img-top" id={id} />
+            </Link>
+            <button className="cart-btn btn btn-sm">Thêm vào giỏ hàng</button>
+          </div>
+          <div className="card-footer d-flex justify-content-center">
+            <ul className="list-group list-group-flush text-center">
+              <StarRating value={3} />
+              <Link to="/" className="link">
+                <li className="list-group-item">
+                  <p className="align-self-center mb-0">{title}</p>
+                </li>
+              </Link>
+              <li className="list-group-item">
+                <h5 className="font-italic mb-0">{price}</h5>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
+  }
 }
