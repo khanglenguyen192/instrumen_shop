70248f9c0035883a6d33c6c0f2d19896bbc2033d
VoMinhTri <trivm2472@gmail.com>
2021-11-03 15:11:53 +0700

    Add PromotionPage

--- src/pages/promotion.js
+++ src/pages/promotion.js
@@ -1,16 +1,30 @@
 import React from "react";
+import Post from "../components/contactComponents/Posts";
+import Title from "../components/title/title";
+import GoodProduct from "../components/contactComponents/goodproduct";
+import BigPost from "../components/PromotionComponent/BigPost";
 
 const Promotion = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "90vh",
-      }}
-    >
-      <h1>Promotion</h1>
+    <div class="container promotion">
+      <Title title="BKIS khuyến mãi"/>
+      <div class="row">
+        <div class="col-12 col-md-8 col-lg-8">
+          <BigPost/>
+        </div>
+        <div class="col-12 col-md-4 col-lg-4">
+          <div class="row">
+            <h3>Bài viết nổi bật</h3>
+            <div class="col-8 col-md-5 col-lg-9 posts">
+              <Post />
+            </div>
+            <h3>Sản phẩm nổi bật</h3>
+            <div class="col-8 col-md-5 col-lg-9 items">
+              <GoodProduct />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
