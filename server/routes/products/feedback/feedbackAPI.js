d72600a8f6e26ba33b71b5de9e341e185f7c78b4
truccm2001 <truccm2001@gmail.com>
2021-11-23 01:48:47 +0700

    add feedback post

--- server/routes/products/feedback/feedbackAPI.js
+++ server/routes/products/feedback/feedbackAPI.js
@@ -9,6 +9,7 @@ router.use((req, res, next) => {
 
 router.get("/", async (req, res) => {
   const id = req.query.id;
+  // const query = `SELECT * FROM ${connection.db_name}.feedback`;
   const query =  `SELECT * FROM ${connection.db_name}.feedback WHERE productID = ?;`
   try {
     connection.db.query(query, [id], (err, results) => {
@@ -23,7 +24,24 @@ router.get("/", async (req, res) => {
 
 
 router.post("/", async (req, res) => {
-  res.send(200);
+  const productID = req.query.productID;
+  const customer_name = req.query.customer_name;
+  const customer_email = req.query.customer_email;
+  const detail = req.query.detail;
+  // const feedbackID = req.query.feedbackID;
+  const time = req.query.time;
+
+  connection.db.query(
+    "INSERT INTO feedback (productID, customer_name, customer_email, detail, time) VALUES (?,?,?,?,?)",
+    [productID, customer_name, customer_email, detail, time],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
 });
 
 router.put("/", async (req, res) => {
