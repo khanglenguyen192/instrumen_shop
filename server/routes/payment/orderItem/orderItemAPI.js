ca74d53e754a47a1d0fd57970c8d8a50e8b94399
Le Nguyen Khang <khang.lenguyen192@gmail.com>
2021-11-25 02:12:28 +0700

    add product detail api

--- server/routes/payment/orderItem/orderItemAPI.js
+++ server/routes/payment/orderItem/orderItemAPI.js
@@ -2,8 +2,8 @@ const { Router } = require("express");
 const express = require("express");
 
 const router = Router();
-router.use(express.json()) // for parsing application/json
-router.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+router.use(express.json()); // for parsing application/json
+router.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 router.use((req, res, next) => {
   next();
 });
@@ -11,7 +11,7 @@ router.use((req, res, next) => {
 const connection = require("../../db_config");
 
 router.get("/", async (req, res) => {
-  var query = `SELECT * FROM ${connection.db_name}.order_item WHERE orderID = ${req.query.ID};`
+  var query = `SELECT * FROM ${connection.db_name}.order_item WHERE orderID = ${req.query.ID};`;
   try {
     connection.db.query(query, (err, results) => {
       const order_item = results;
@@ -22,16 +22,15 @@ router.get("/", async (req, res) => {
   }
 });
 router.post("/", async (req, res) => {
-  for(let i = 0; i < req.body.length; i++)
-  {
-  var query = `INSERT INTO ${connection.db_name}.order_item (orderID,productID,quantity) VALUES ((SELECT MAX(orderID) FROM ${connection.db_name}.order), ${req.body[i].props.id}, ${req.body[i].props.amount});`;
-  try {
-    connection.db.query(query, (err, results) => {
-      console.log("Sent order-item");
-    });
-  } catch (err) {
-    console.log("ERROR: " + err);
-  }
+  for (let i = 0; i < req.body.length; i++) {
+    var query = `INSERT INTO ${connection.db_name}.order_item (orderID,productID,quantity) VALUES ((SELECT MAX(orderID) FROM ${connection.db_name}.order), ${req.body[i].props.id}, ${req.body[i].props.amount});`;
+    try {
+      connection.db.query(query, (err, results) => {
+        console.log("Sent order-item");
+      });
+    } catch (err) {
+      console.log("ERROR: " + err);
+    }
   }
 });
 
